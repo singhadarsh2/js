85586cb8fb006e04dd887501b7ff794337ef57fe
singhadarsh2 <147526776+singhadarsh2@users.noreply.github.com>
2025-02-27 08:06:31 +0000

    conversion of data types

---
+++ 02_basics/conversion.js
@@ -0,0 +1,23 @@
+let age =10
+let changeage = String(age)
+console.log(age)
+console.log(typeof age)
+
+
+let value=5
+let negvalue=-value
+console.log(negvalue)
+console.log(value**value)
+
+console.log("laksh"+"singh")
+console.log(2+"laksh")
+console.log("1"+2)
+console.log(2+"3")
+console.log("1"+"2")
+console.log("laksh"+2)       // here all the concatinate due to string as like first ek laksh+singh=lakshsingh all are same
+console.log("1"+2+"2")       //contatinate just like string while in the last example if the number comes first then
+console.log("1"+2+2)         // it will add first then concatinate ex- 1+2+"3" = 33
+console.log(1+"2"+2)
+console.log(1+"2"+"2")
+console.log(1+"2"+"2"+3)
+console.log(1+2+"3")
