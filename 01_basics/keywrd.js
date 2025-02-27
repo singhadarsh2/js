4b7579d67be68c7095c91119c090316d0b2a4398
singhadarsh2 <147526776+singhadarsh2@users.noreply.github.com>
2025-02-27 06:40:21 +0000

    variable and keywords

---
+++ 01_basics/keywrd.js
@@ -0,0 +1,10 @@
+const accntId =12345
+let accntName="laksh"
+var accntCity="jaipur"
+account="punjab"
+let accountstate;
+console.table([accntId,accntName,accntCity,account,accountstate]);
+
+//if you want to change value of these keyword//
+accntId=254
+console.log(accntId)
---
+++ 01_basics/variable.js
@@ -0,0 +1,25 @@
+"use strict" //treat all code as javascript newer version
+
+//alert(3+3)- we are using node js not browser so node js have different symbol for this.
+
+
+console.log(3
+    +
+    3
+); // in js space is ignored by js but it is not good practice readability issue
+
+
+
+
+let name="laksh" // String dataType
+let age = 20 // Integer dataType
+let isLoggedin = false // boolean
+let state //undefined-means value is not assigned yet
+let ram =null // standalone value means 
+
+//DataTypes -> number,bigint,string,boolean,undefined,null,symbol
+ 
+//Objects in js
+console.log(typeof age);
+console.log(typeof ram);
+console.log(typeof null);
